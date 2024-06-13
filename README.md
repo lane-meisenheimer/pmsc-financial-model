4fa9d073a990a0910866f783919d17925abe8a59
Lane Meisenheimer <rlmeisenheimer@gmail.com>
2024-06-12 20:01:28 -0700

    feat: add usage guide and command line params

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 
 # financial-model
+## Introduction
+The PMSC financial model allows you to generate projected expenses for PMSC based on the previous
+expenses from the 2023-2024 school year.  
+
+The simulation will adjust based on *student count*, *teacher count*, and *the number of subjects*.
+> Note that the default number of subjects in the PMSC base curriculum is **5**:
+> 1. Life Dream
+> 2. Self Awareness
+> 3. Cognitive Construction
+> 4. Interpersonal Relationships
+> 5. Coping
+
+The baseline simulation commited to the repository was run with the following parameters
+
+| Parameter          | Value  |
+|--------------------|--------|
+| Min Students       | 1,000  |
+| Max Students       | 10,000 |
+| Number of Subjects | 6      |
 
 ## Simulation
-`npx ts-node bin/simulateCosts.ts`
+In order to re-reun the simulation and recreate **data/simulated-averages.json** and **data/simulated-years.json** u
+update the values in **bin/simulateCosts.ts** and run the following command.
+
+`npx ts-node bin/simulateCosts.ts -c <number of subject> -m <min students> -x <max students>`
+
+### Example
+
+`npx ts-node ./bin/simulateCosts.ts -m 1000 -x 10000 -c 6`
--- bin/simulateCosts.ts
+++ bin/simulateCosts.ts
@@ -1,17 +1,69 @@
 import {writeFile} from "fs/promises";
 import {join} from "path";
 import simulatePMSCCosts from "../lib/simulatePMSCCosts";
+import commandLineArgs, {OptionDefinition} from "command-line-args";
+import commandLineUsage from 'command-line-usage'
+
+const optionDefinitions:OptionDefinition[] = [
+    { name: 'min-students', alias: 'm', type: Number, defaultValue: 1000 },
+    { name: 'max-students', alias: 'x', type: Number, defaultValue: 10000 },
+    { name: 'subject-count', alias: 'c', type: Number, defaultValue: 5 },
+    {name: 'help', alias: 'h', type: Boolean, defaultValue: false},
+];
+
+const options = commandLineArgs(optionDefinitions)
+
+const sections = [
+    {
+        header: 'PMSC Financial Model Simulator',
+        content: 'Generates simulate data based on the 2023-2024 school year costs.'
+    },
+    {
+        header: 'Options',
+        optionList: [
+            {
+                name: 'min-students',
+                typeLabel: '{underline number} ',
+                alias: 'm',
+                description: 'The {bold minimum} number of students to start the simulation.\n'
+            },
+            {
+                name: 'max-students',
+                typeLabel: '{underline number} ',
+                alias: 'x',
+                description: 'The {bold maximum} number of students to start the simulation.\n'
+            },
+            {
+                name: 'subject-count',
+                typeLabel: '{underline number} ',
+                alias: 'c',
+                description: 'The number of subjects which the simulation should use {bold Note that the default amount is 5}.\n'
+            },
+            {
+                name: 'help',
+                alias: 'h',
+                description: 'Print this usage guide.'
+            }
+        ]
+    }
+]
+const usage = commandLineUsage(sections)
 
 const main = async () => {
-    let studentCount = 1000;
-    const subjectsWithFinancialLiteracy = 6;
+    if(options["help"]) {
+        console.log(usage);
+        return;
+    }
+
+    let studentCount = options["min-students"];
+
     const averageCosts = [];
     const simulatedYearsByCount = [];
-    while (studentCount <= 10000) {
+    while (studentCount <= options["max-students"]) {
         const {averageMonthlyCost, simulatedMonthlyCosts} = simulatePMSCCosts({
             numberOfStudents: studentCount,
             numberOfTeachers: studentCount / 10,
-            numberOfSubjects: subjectsWithFinancialLiteracy
+            numberOfSubjects: options["subject-count"]
         });
 
         averageCosts.push({students: studentCount, cost: averageMonthlyCost});
